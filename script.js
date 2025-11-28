360f8c96ee6a5488c5d9be0399483e5983246002
chloie4567 <maranon_chloie@fbc.ph.education>
2025-11-28 00:27:47 +0000

    respiratory

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('change-color-btn').addEventListener('click', function() {
+      const learned = document.getElementById('learned');
+        if (learned.style.display === 'block') {
+            learned.style.display = 'none';
+              } else {
+                  learned.style.display = 'block';
+                    }
+                    });
+
+                    
+})
